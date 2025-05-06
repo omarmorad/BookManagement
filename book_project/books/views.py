918ce2ec06a00089047f3f62007cee1dac7aaac4
omarmorad <omorad5362@gmail.com>
2025-05-06 15:45:14 +0300

    feat(book-management): add create, update, and delete functionality for books
    Introduce new views and templates to support creating, editing, and deleting books. This includes:
    - BookCreateView, BookUpdateView, and BookDeleteView
    - book_form.html and book_confirm_delete.html templates
    - Updated URLs and book_list.html to include management actions

--- book_project/books/views.py
+++ book_project/books/views.py
@@ -1,11 +1,13 @@
-from django.views.generic import ListView, DetailView
-from .models import Book, Author
+from django.db import models
 from django.shortcuts import render
-
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Book, Author
+from django import forms
 def homepage(request):
     return render(request, 'home.html')
 
-# Book Views
+# Book views
 class BookListView(ListView):
     model = Book
     template_name = 'book_list.html'  # Changed from 'books/book_list.html'
@@ -16,7 +18,7 @@ class BookDetailView(DetailView):
     template_name = 'book_detail.html'  # Changed from 'books/book_detail.html'
     context_object_name = 'book'
 
-# Author Views
+# Author views
 class AuthorListView(ListView):
     model = Author
     template_name = 'author_list.html'  # Changed from 'books/author_list.html'
@@ -26,8 +28,35 @@ class AuthorDetailView(DetailView):
     model = Author
     template_name = 'author_detail.html'
     context_object_name = 'author'
-
+    
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['books'] = self.object.books.all()
+        context['books'] = Book.objects.filter(author=self.object)
         return context
+
+# New views for book management
+class BookForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = ['title', 'author', 'publish_date', 'price', 'appropriate_age', 'image']
+        widgets = {
+            'author': forms.Select(attrs={'class': 'form-select'}),
+            'appropriate_age': forms.Select(attrs={'class': 'form-select'}),
+        }
+
+class BookCreateView(CreateView):
+    model = Book
+    form_class = BookForm
+    template_name = 'book_form.html'
+    success_url = reverse_lazy('book-list')
+
+class BookUpdateView(UpdateView):
+    model = Book
+    form_class = BookForm
+    template_name = 'book_form.html'
+    success_url = reverse_lazy('book-list')
+
+class BookDeleteView(DeleteView):
+    model = Book
+    template_name = 'book_confirm_delete.html'
+    success_url = reverse_lazy('book-list')
--- book_project/config/urls.py
+++ book_project/config/urls.py
@@ -4,19 +4,26 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from books.views import (
-    BookListView, 
-    BookDetailView, 
-    AuthorListView, 
-    AuthorDetailView,
-    homepage
+    BookListView, BookDetailView, 
+    AuthorListView, AuthorDetailView,
+    BookCreateView, BookUpdateView, BookDeleteView
 )
 
 urlpatterns = [
-    path('', homepage, name='home'),
+    path('admin/', admin.site.urls),
+    path('', lambda request: render(request, 'home.html'), name='home'),
     path('books/', BookListView.as_view(), name='book-list'),
     path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
     path('authors/', AuthorListView.as_view(), name='author-list'),
     path('authors/<int:pk>/', AuthorDetailView.as_view(), name='author-detail'),
-    path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
+    # New URLs for book management
+    path('books/new/', BookCreateView.as_view(), name='book-create'),
+    path('books/<int:pk>/edit/', BookUpdateView.as_view(), name='book-edit'),
+    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),
+]
+
+# Add this if you haven't already to serve media files during development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
